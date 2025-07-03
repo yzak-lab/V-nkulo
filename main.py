b7dc34c51a77d9d0744704c3358f747bdcc3297c
yzak-lab <yzak_erez@hotmail.com>
2025-07-02 19:51:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from routers import auth
 app = FastAPI()
 
 app.include_router(auth.router)
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def root():
+  return {"message": "Hola desde Vínkulo"}
